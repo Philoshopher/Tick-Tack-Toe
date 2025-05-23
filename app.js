bfb9badac7197c459b91658c343de04f085d2ee5
Philoshopher <prathmeshchauhan2304@gmail.com>
2025-05-23 18:08:54 +0530

    Added win checker with message functionality in case of winner

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // DOM elemrnts
 let boxes = document.querySelectorAll('.box');
 let reset = document.querySelector('.reset-btn');
-
+let msg = document.querySelector('.msg');
+let count = 0;
 const wPatterns = [
   [0, 1, 2],
   [3, 4, 5],
@@ -25,16 +26,41 @@ boxes.forEach((box) => {
       player = true;
       box.classList.add('cross');
     }
+    count++;
     box.disabled = true;
+    console.log(winCheck());
   });
 });
 
 reset.addEventListener('click', () => {
   boxes.forEach((box) => {
     box.innerText = '';
+    msg.innerHTML = ``;
     box.classList.remove('circle');
     box.classList.remove('cross');
     box.disabled = false;
     player = true;
   });
 });
+
+function winCheck() {
+  for (let pattern of wPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != '' && pos2Val != '' && pos3Val != '') {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        winner(pos1Val);
+        return true;
+      }
+    }
+  }
+}
+
+function winner(text) {
+  msg.classList.remove('.hide');
+  msg.innerHTML = `<p>${text} is the winner</p>`;
+  reset.innerHTML = 'Play Again';
+  reset.classList.add('.playAgain');
+}
